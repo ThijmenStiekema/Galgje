954f2115d56e9c7c07c648c024f0073e810ca9f4
Danto Rosier <>
2021-05-26 11:56:34 +0000

    computer kiest woord uit lijst

---
+++ main.py
@@ -0,0 +1,11 @@
+words = ["informatica","informatiekunde","spelletje","aardigheidje","scholier","fotografie","waardebepaling","specialiteit","verzekering","universiteit","heesterperk"]
+  
+#import random module package  
+import random
+
+def game():
+  woord = random.choice(words)
+  print(woord)
+
+game()
+
