5ce78d4159b532dce7faa12a7ad0381a4a11be13
ThijmenStiekema <>
2021-06-05 16:54:40 +0000

    Geeft nu aan welke letters al zijn gekozen en blokkeert al gekozen letters, ook blokkeert hij antwoorden die geen letters zijn

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ words = ["informatica","informatiekunde","spelletje","aardigheidje","scholier","
 import random
 
 
-print("ahllo".find("l"))
+print("hallo".find("l"))
 
 fouten = 0
 
+
 def kiezen():
     #Checkt of de speler game over is en herstart de game
-  
   global fouten  
   if fouten == 12:
     fouten = 0
@@ -20,8 +20,18 @@ def kiezen():
     game()
   
   else:
+    print("Al gekozen letters:") 
+    print(*al_gekozen_letters)
+
     gekozen_letter = input("kies een letter: ")
-    if gekozen_letter in woord:
+    if gekozen_letter in al_gekozen_letters:
+      print("Letter is al gekozen")
+      kiezen()
+
+    elif gekozen_letter.lower() in woord and gekozen_letter.isalpha():
+      al_gekozen_letters.append(gekozen_letter)
+
+      #Geeft de locatie van de letters in het woord aan
       index = 0
       while index < len(woord):
         index = woord.find(gekozen_letter, index)
@@ -29,23 +39,28 @@ def kiezen():
             break
         print(index)
         index += 1
-
-    
       print("Goed gedaan!")
       kiezen()
-    else:
+
+    elif gekozen_letter.isalpha():
+      al_gekozen_letters.append(gekozen_letter)
       print("Fout")
       fouten += 1
       kiezen() 
-      
+  
+    else: 
+      print ("Antwoord is niet een letter of woord")
+      kiezen()
 
 def game():
   global woord
   #Kiest woord en zet streepjes neer
   woord = random.choice(words)
   lengte = len(woord)
-  print("_ "*lengte)
-
+  print("_ " * lengte)
+  global al_gekozen_letters
+  al_gekozen_letters = []
+  
   kiezen()
   
 game()
