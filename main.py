f00514c848a75c2b8ae9343da4c27e3005d236d2
ThijmenStiekema <>
2021-06-05 18:24:16 +0000

    Toegevoegd dat als alle letters zijn geraden dat win U de game wint en fouten teller toe gevoegd

--- main.py
+++ main.py
@@ -4,28 +4,39 @@ words = ["informatica","informatiekunde","spelletje","aardigheidje","scholier","
 #import random module package 
 import random
 
-
-print("hallo".find("l"))
-
+goede_letters = 0
 fouten = 0
+
+def game():
+  global goede_letters
+  goede_letters = 0
+  global woord
+  #Kiest woord en zet streepjes neer
+  woord = random.choice(words)
+  global lengte
+  lengte = len(woord)
+  global gecodeerd_woord
+  gecodeerd_woord = ["_ "] * lengte
+  global al_gekozen_letters
+  al_gekozen_letters = []
+  kiezen()
 
 
 def kiezen():
   print(*gecodeerd_woord)
   #Checkt of de speler game over is en herstart de game
   global fouten
-  
   if fouten == 12:
     fouten = 0
     print("Game Over")
     input("press enter to continue ")
     game()
-  
+
   else:
     #print gekozen letters
     print("Al gekozen letters:") 
     print(*al_gekozen_letters)
-    print()
+    print("fouten: "+ str(fouten))
     gekozen_letter = input("kies een letter: ")
     if gekozen_letter in al_gekozen_letters:
       print("Letter is al gekozen")
@@ -43,9 +54,14 @@ def kiezen():
         index += 1
         gecodeerd_woord[index-1] = gekozen_letter
       print("Goed gedaan!")
+      if gecodeerd_woord.count("_ ") == 0:
+        print("GG")
+        input("press enter to continue ")
+        print(woord)
+        game()
+      else:
+        kiezen()
       
-      kiezen()
-
     elif gekozen_letter.isalpha():
       al_gekozen_letters.append(gekozen_letter)
       print("Fout")
@@ -56,19 +72,5 @@ def kiezen():
       print ("Antwoord is niet een letter of woord\n")
       kiezen()
 
-def game():
-  global woord
-  #Kiest woord en zet streepjes neer
-  woord = random.choice(words)
-  lengte = len(woord)
-  global gecodeerd_woord
-  gecodeerd_woord = ["_ "] * lengte
-  global al_gekozen_letters
-  al_gekozen_letters = []
-  
-  kiezen()
-  
 game()
 #Start de game
-
-      
